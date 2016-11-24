bb01a133e8002c9c941513cae81e94afa09ac08a
aneury1 <aneury1@gmail.com>
2016-11-24 15:55:13 -0400

    actualizar Practica 4

--- practica4.sql
+++ practica4.sql
@@ -1,1 +1,14 @@
 ---practica 4
+
+
+
+INSERT INTO Escuela values
+(
+   1,
+   'Ab escuela ',
+   'c/ francisco prat ramires'
+)
+
+
+
+insert into Escuela select  NombreEscuela, DomicilioEscuela from escuela where CodigoEscuela == 1 
