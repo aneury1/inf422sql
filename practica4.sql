b9aa7da6194d68fb79ece11edc684382df77f815
aneury1 <aneury1@gmail.com>
2016-11-24 16:05:09 -0400

    actualizar Practica 4, final , sin Extras...

--- practica4.sql
+++ practica4.sql
@@ -9,6 +9,22 @@ INSERT INTO Escuela values
    'c/ francisco prat ramires'
 )
 
+insert into Escuela select  NombreEscuela, DomicilioEscuela from escuela where CodigoEscuela == 1 ;
 
+delete from telefono_Escuela where TelefonoEscuela <> '1111-1111'
 
-insert into Escuela select  NombreEscuela, DomicilioEscuela from escuela where CodigoEscuela == 1 
+update telefono_Escuela set TelefonoEscuela = '5445-3223' where TelefonEscuela = '0000-0000'
+
+update Guia set CodigoGuia = 2 where CodigoGuia == 1
+
+delete from ReservaTipoVisita where CantidadReserva < 10
+
+delete from guias where NomGuia = null
+
+select from guias where nomGui = 'bernardo'
+
+select * from reservas;
+
+select From reservas where CantidadAlumnosReservados > 20
+
+SELECT * FROM reservas WHERE  FechaVisitaReservada ='3-6-2004'
