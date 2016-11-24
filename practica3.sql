ee438eec0daa76c8d6407540759d93200247a04a
aneury1 <aneury1@gmail.com>
2016-11-24 15:32:14 -0400

    actualizando el fichero de practica 3

--- practica3.sql
+++ practica3.sql
@@ -25,3 +25,29 @@ CREATE TABLE CLIENTE
 )
 
 
+CREATE TABLE PROVEDORES 
+(
+   IdProvedores integer not null primary key,
+   NomProveedor varchar(40) not null, 
+   dirProveedor varchar (40) not null, 
+   nomContacto varchar (80) not null, 
+   cargoContacto varchar(80) not null, 
+   CONSTRAINT fk_id_pais_proveedor FOREIGN KEY( IdPais) references PAISES(IdPais), 
+   fonoProveedor varchar(15) not null, 
+   faxProveedor varchar(15) not null
+)
+
+
+CREATE TABLE PRODUCTOS 
+(
+   IdProducto integer not null primary key, 
+   NombreProducto varchar(80) not null,
+   constraint fk_id_proveedor foreign key(IdProvedores) references PROVEDORES(idProveedor), 
+   constraint fk_id_Categoria foreign key(IDCategoria)   references CATEGORIA(IDCategoria),
+   cantxUnidad integer not null,
+   precioPorUnidad DECIMAL(10, 2) NOT NULL, 
+   UnidadExistencia smallint not null,
+   UnidadPedido smallint not null
+)
+
+
